1db3c05082031bf3831408b2616cc1597053d694
y4ss <40444225+y4ss@users.noreply.github.com>
2019-03-22 16:13:46 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -8,22 +8,18 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
-setup(
-    name="PySAP_SL",
-    version="0.1.0",
-    description="Automation tool for source list in SAP",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/y4ss/PySAP_SL",
-    author="Yacine BEKKA",
-    author_email="yacinebekka@yahoo.fr",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Windows",
-    ],
-    packages=find_packages(exclude=("test",),
-    include_package_data=True,
-    install_requires=["win32com.client", "copy"],
-)
+setup(name="PySAP_SL",
+version="0.1.0",
+description="Automation tool for source list in SAP",
+long_description=README,
+long_description_content_type="text/markdown",
+url="https://github.com/y4ss/PySAP_SL",
+author="Yacine BEKKA",
+author_email="yacinebekka@yahoo.fr",
+license="MIT",
+classifiers=["License :: OSI Approved :: MIT License",
+"Programming Language :: Python :: 3",
+"Operating System :: Windows",],
+packages=find_packages(exclude=("test",)),
+include_package_data=True,
+install_requires=["win32com.client", "copy"])
